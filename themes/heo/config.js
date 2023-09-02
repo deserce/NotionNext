ff3fcffe3f03b1712d670f8940b4211e836e49ca
deserce <deserce@163.com>
2023-09-03 00:05:01 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -1,21 +1,21 @@
 const CONFIG = {
   HOME_BANNER_ENABLE: true,
 
-  SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
+  SITE_CREATE_TIME: '2020-04-01', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到我的博客', url: 'https://notion.deserce.top/'},
+ 
   ],
 
   // 英雄区(首页顶部大卡)
-  HERO_TITLE_1: '分享编程',
-  HERO_TITLE_2: '与思维认知',
-  HERO_TITLE_3: 'TANGLY1024.COM',
-  HERO_TITLE_4: '新版上线',
-  HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HERO_TITLE_LINK: 'https://tangly1024.com',
+  HERO_TITLE_1: '分享热爱',
+  HERO_TITLE_2: '投资理财',
+  HERO_TITLE_3: '陶博士',
+  HERO_TITLE_4: '选对股买对时机',
+  HERO_TITLE_5: '三七之路_2023',
+  HERO_TITLE_LINK: 'https://notion.deserce.top/article/4edd4108-9db5-4204-8f28-31751f51c042',
 
   // 英雄区显示三个置顶分类
   HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
@@ -30,13 +30,12 @@ const CONFIG = {
   INFOCARD_GREETINGS: [
     '你好！我是',
     '🔍 分享与热心帮助',
-    '🤝 专修交互与设计',
     '🏃 脚踏实地行动派',
     '🏠 智能家居小能手',
     '🤖️ 数码科技爱好者',
     '🧱 团队小组发动机'
   ],
-  INFO_CARD_URL: 'https://github.com/tangly1024/NotionNext', // 个人资料底部按钮链接
+  INFO_CARD_URL: 'https://github.com/deserce/NotionNext', // 个人资料底部按钮链接
 
   // 用户技能图标
   GROUP_ICONS: [
@@ -102,7 +101,7 @@ const CONFIG = {
   SOCIAL_CARD_TITLE_1: '交流频道',
   SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
   SOCIAL_CARD_TITLE_3: '点击加入社群',
-  SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  SOCIAL_CARD_URL: 'https://join.skype.com/klC8HQ5pFrXO',
 
   // *****  以下配置无效，只是预留开发 ****
   // 菜单配置
