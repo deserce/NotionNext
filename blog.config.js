76a0ab4a9280d951c69b7c5238f78194ce55364a
mingcy <117440588+rumian0@users.noreply.github.com>
2024-04-14 00:06:27 +0800

    Update blog.config.js
    更改了原地址

--- blog.config.js
+++ blog.config.js
@@ -17,9 +17,9 @@ const BLOG = {
 
       CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-      AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'mingcy', // 您的昵称 例如 tangly1024
+      AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '茗辰原', // 您的昵称 例如 tangly1024
       BIO: process.env.NEXT_PUBLIC_BIO || '一个普通的干饭人🍚', // 作者简介
-      LINK: process.env.NEXT_PUBLIC_LINK || 'https://mcy.cloudns.org/', // 网站地址
+      LINK: process.env.NEXT_PUBLIC_LINK || 'https://mingcy.xyz/', // 网站地址
       KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客, 茗辰原,安卓,电脑,app,软件,黑科技,美景,可爱,茗辰原の异世界,茗辰原,c++', // 网站关键词 英文逗号隔开
 
       // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
