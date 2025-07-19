02e5a80745f7e289abf376b273c607b010134785
deserce <deserce@163.com>
2025-07-19 21:47:36 +0800

    Update blog.config.js
    https://notion.deserce.dpdns.org/更新域名

--- blog.config.js
+++ blog.config.js
@@ -19,7 +19,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'deserce', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '一个普通的干饭人🍚', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://notion.deserce.top', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://notion.deserce.dpdns.org/', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
